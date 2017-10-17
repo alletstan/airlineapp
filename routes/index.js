0d263ee3693c46b92220b28292c9dc42e35c4eb7
yeong jinzhi <yeong_jz@hotmail.com>
2017-10-17 13:07:50 +0800

    made splitter navigatable

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/main.html', function(req, res, next) {
   res.render('main', {});
 });
 
+router.get('/about.html', function(req, res, next) {
+  res.render('about', {});
+});
+
+router.get('/settings.html', function(req, res, next) {
+  res.render('settings', {});
+});
+
 module.exports = router;
