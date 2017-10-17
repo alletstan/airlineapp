73ee81f5198f9230903d962eb210f7a3a568e8b6
yeong jinzhi <yeong_jz@hotmail.com>
2017-10-17 13:38:42 +0800

    added other pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/logout.html', function(req, res, next) {
+  res.render('logout', {});
 });
 
 router.get('/main.html', function(req, res, next) {
@@ -14,8 +18,32 @@ router.get('/about.html', function(req, res, next) {
   res.render('about', {});
 });
 
-router.get('/settings.html', function(req, res, next) {
-  res.render('settings', {});
+router.get('/qrCode.html', function(req, res, next) {
+  res.render('qrCode', {});
+});
+
+router.get('/packageDetails.html', function(req, res, next) {
+  res.render('packageDetails', {});
+});
+
+router.get('/attractions.html', function(req, res, next) {
+  res.render('attractions', {});
+});
+
+router.get('/specialEvents.html', function(req, res, next) {
+  res.render('specialEvents', {});
+});
+
+router.get('/recommended.html', function(req, res, next) {
+  res.render('recommended', {});
+});
+
+router.get('/lepak.html', function(req, res, next) {
+  res.render('lepak', {});
+});
+
+router.get('/emergencyContacts.html', function(req, res, next) {
+  res.render('emergencyContacts', {});
 });
 
 module.exports = router;
