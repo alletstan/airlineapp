3c357fc0f8757795e09740d8e342873b0ec58caf
sohjunjie <junjie.soh93@gmail.com>
2017-10-19 08:43:53 +0800

    remove body head from pages

--- airlineapp/www/javascripts/accordion.js
+++ airlineapp/www/javascripts/accordion.js
@@ -1,14 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
+$(document).on('click', '.accordion', function(event) {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+      panel.style.display = "none";
+  } else {
+      panel.style.display = "block";
+  }
+});
