2fb505196d880ce9c928edc57a199c6cb4c26469
sohjunjie <junjie.soh93@gmail.com>
2017-10-21 19:28:53 +0800

    fix cors using 3rd party

--- airlineapp/www/lib/common/app.script.js
+++ airlineapp/www/lib/common/app.script.js
@@ -1,4 +1,13 @@
-var domain_url = "http://localhost:3000/";
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
+var krisFlyerNumber = null;
+var userProfile = null;
 
 window.fn = {};
 window.fn.open = function() {
@@ -13,45 +22,44 @@ window.fn.load = function(page) {
 
 var login = {};
 login.goToMainIfAuthenticated = function() {
-  $.ajax({
-    type: "GET",
-    url: domain_url + 'api/checkiflogin',
-    success: function(data, status){
-      if(data.islogin){
-        document.querySelector('#mainNavigator').resetToPage('main.html');
-      }
-    },
-    error: function(err, xhr) {
-      console.log(err);
-    }
-  });
+
+  if(krisFlyerNumber == null) {
+    document.querySelector('#mainNavigator').resetToPage('main.html');
+  }
+
 }
 login.tryLogin = function() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  payload = {username: username, password: password};
+  let enteredKrisNumber = String(document.getElementById('username').value);
+  let payload = "{\n  \"request\": {\n    \"krisflyerNumber\": \"" + enteredKrisNumber + "\"\n  },\n  \"clientUUID\": \"<ChangeThis: AnyUniqueStringForYourTeam>\"\n}"
+
   $.ajax({
-    type: "POST",
+    method: "POST",
     data: payload,
-    url: domain_url + 'api/login',
-    success: function(status){
+    dataType: 'json',
+    headers: {
+      "content-type": "application/json",
+      "x-api-key": "du1yO8KLZm9PfFeg6OHQW8CFcpK1RMym3JXp78Uk",
+      "cache-control": "no-cache",
+      "postman-token": "12347e3a-c45f-ed6a-6290-c2d9648c7b59"
+    },
+    crossDomain: true,
+    url: "https://apidev.singaporeair.com/appchallenge/krisflyer/getprofile",
+    success: function(data, status){
+      krisFlyerNumber = enteredKrisNumber;
+      userProfile = data;
       document.querySelector('#mainNavigator').resetToPage('main.html');
     },
     error: function(err, xhr) {
-      ons.notification.alert('Incorrect username or password.');
+      console.log(err);
+      ons.notification.alert('Invalid kris flyer account number.');
     }
   });
 };
 
 var logout = function() {
-  $.ajax({
-    type: "GET",
-    url: domain_url + 'api/logout',
-    success: function(status){
-      document.querySelector('#mainNavigator').resetToPage('login.html', {});
-    },
-    error: function(err, xhr) {
-      console.log(err);
-    }
-  });
+
+  krisFlyerNumber = null;
+  userProfile = null;
+  document.querySelector('#mainNavigator').resetToPage('login.html', {});
+
 };
