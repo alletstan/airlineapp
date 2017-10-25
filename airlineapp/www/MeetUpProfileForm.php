57d605a1e687887c9a7d1b637e27446f8cc78605
Eimoh <eimoh192@gmail.com>
2017-10-25 14:08:04 +0800

    profile edit

---
+++ airlineapp/www/MeetUpProfileForm.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+  Name: <?php echo $_POST["name"]; ?><br>
+  Display Name: <?php echo $_POST["displayname"]; ?>
+  Nationality: <?php echo $_POST["nationality"]; ?>
+  Short Display Message: <?php echo $_POST["displaymessage"]; ?>
+
+</body>
+</html>
