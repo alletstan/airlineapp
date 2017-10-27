9bab8d0cd0ef08cca00b16481ea1a3483728a9c8
dhecloud <andrewkohjinjie@hotmail.com>
2017-10-27 17:16:40 +0800

    added cors for feb

--- airlineapp/app.js
+++ airlineapp/app.js
@@ -19,12 +19,13 @@
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
 var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
-
+var cors = require('cors')
 var app = express();
 
 // Bootstrap application settings
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
+app.use(cors());
 
 // Create the service wrapper
 var conversation = new Conversation({
